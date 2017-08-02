79df9302ec344fd81a0441a6b3a3add7f39702eb
Jordan Munroe <munroejr@mcmaster.ca>
2017-08-01 21:56:56 -0400

    hide overflow.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import createTitle from './components/title'
 import createBackground from './components/background'
 
-document.body.style.margin = 0
+document.body.style.margin = '0'
+document.body.style.overflow = 'hidden'
 createTitle('JORDAN MUNROE')
 createBackground()
