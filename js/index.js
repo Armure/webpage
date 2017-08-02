44805c6b1bc3e52451dc6d1a9e68f2df986469a4
Jordan Munroe <munroejr@mcmaster.ca>
2017-08-01 21:58:58 -0400

    really hide overflow.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ import createTitle from './components/title'
 import createBackground from './components/background'
 
 document.body.style.margin = '0'
+document.body.style.width = '100%'
+document.body.style.height = '100%'
 document.body.style.overflow = 'hidden'
 createTitle('JORDAN MUNROE')
 createBackground()
